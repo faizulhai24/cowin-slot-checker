1614791a020fe38dea6b15fac600930300cd0406
Faiz <faizulhai24@gmail.com>
2021-05-04 10:52:08 +0530

    Added admin interface

--- cowin/core/admin.py
+++ cowin/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, District
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ['updated_at']
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('updated_at', 'to_time', 'surge_price', 'surge_role', 'minimum_tickets', 'ended')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(District, DistrictAdmin)
