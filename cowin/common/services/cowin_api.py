e4a088b313ec006905bd34708e7cb4f171e95459
Faiz <faizulhai24@gmail.com>
2021-05-04 07:39:37 +0530

    Added a log

--- cowin/common/services/cowin_api.py
+++ cowin/common/services/cowin_api.py
@@ -10,9 +10,9 @@ class CowinApi:
         self.URL_BASE = "https://cdn-api.co-vin.in"
 
     def check_slots_by_district_and_date(self, district_id, date):
-        resp = requests.get(
-            self.URL_BASE + "/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(
-                district_id, date))
+        url = self.URL_BASE + "/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(district_id, date)
+        logger.info(url)
+        resp = requests.get(url)
         if resp.status_code != 200:
             logger.exception("Cowin API failed, {}, {}".format(resp.url, resp.status_code))
             return None
